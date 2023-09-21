0ac145840522bc3c8cb3e43576b8c8780be9f0bb
hongwx <hong_wx@i-hengfeng.com>
2023-09-21 10:35:07 +0800

    更新插件

--- uni_modules/hf-middleware/js_sdk/api/uni.js
+++ uni_modules/hf-middleware/js_sdk/api/uni.js
@@ -172,11 +172,13 @@ export function chooseLocation({ latitude = '', longitude = '', coordinateSystem
 		// 微信小程序 默认打开腾讯地图 坐标系: gcj02
 		// APP 默认打开高德地图 坐标系: gcj02
 		// uni.chooseLocation 返回经纬度 使用 gcj02 国测局坐标系
-		[latitude, longitude] = mapConversion(longitude, latitude, coordinateSystem, coord);	// 坐标系转换
+		[longitude, latitude] = mapConversion(longitude, latitude, coordinateSystem, coord);	// 坐标系转换
+		// console.log('--- uni.chooseLocation 前 --->', longitude, latitude, coord)
 		uni.chooseLocation({
 			latitude,
 			longitude,
 			success: (res) => {
+				// console.log('--- uni.chooseLocation --->', res, coord)
 				const [lat, lng] = mapConversion(res.longitude, res.latitude, coord, coordinateSystem);
 				resolve({
 					latitude: lat,
